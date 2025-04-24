05c7094b289590307f28f8d2b99b3ca015c0ae5d
Laura <laura.p.amaral@aluno.senai.br>
2025-04-24 14:54:22 -0300

    Atualizando os Métodos pt2

--- src/components/cadastro/Cadastro.jsx
+++ src/components/cadastro/Cadastro.jsx
@@ -7,18 +7,18 @@ const Cadastro = () => {
             <form action="" className="layout_grid form_cadastro">
                 <h1>Cadastro de Filme</h1>
                <hr/>
-                <div>
-                    <div>
+                <div className="campos_cadastro">
+                    <div className="campo_cad_nome">
                         <label htmlFor="">Nome</label>
-                        <input type="text" name="nome"></input>
+                        <input type="text" name="nome" placeholder="Digite seu nome: "></input>
                     </div>
-                    <div>
+                    <div className="campo_cad_genero">
                         <label htmlFor="genero">Gênero</label>
                         <select name="genero" id=""> 
                             <option value="" disabled selected> Selecione </option>
-                            <option value="" >op 1 </option>
-                            <option value="" >op 2 </option>
-                            <option value="" >op 3 </option>
+                            <option value="" > op 1 </option>
+                            <option value="" > op 2 </option>
+                            <option value="" > op 3 </option>
                         </select>
                     </div>
                     <Botao/>
