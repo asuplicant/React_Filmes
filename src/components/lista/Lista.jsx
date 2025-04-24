b602361c5af87b6341e4bbc87697fee57171218e
Laura <laura.p.amaral@aluno.senai.br>
2025-04-24 16:50:47 -0300

    Atualizandoooogit add .git add .

--- src/components/lista/Lista.jsx
+++ src/components/lista/Lista.jsx
@@ -1,8 +1,12 @@
 import "./Lista.css";
 
+// Importação de imagens:
+import Editar from "../../assets/img/pen-to-square-solid.svg";
+import Excluir from "../../assets/img/trash-can-regular.svg";
+
 const Lista = () => {
     return(
-        <section>
+        <section className="layout_grid listagem">
             <h1> Lista dos Filmes </h1>
             <hr/>
 
@@ -21,7 +25,12 @@ const Lista = () => {
                     </thead>
                     {/* tbody => Corpo da Tabela */}
                     <tbody>
-                        
+                        <tr className="item_lista">
+                            <td> Coraline </td>
+                            <td> Terror </td>
+                            <td><img src={Editar} alt="Caneta" /></td>
+                            <td><img src={Excluir} alt="Lixeira" /></td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
--- src/pages/cadastroFilme/CadastroFilme.jsx
+++ src/pages/cadastroFilme/CadastroFilme.jsx
@@ -2,6 +2,7 @@
 import Cadastro from "../../components/cadastro/Cadastro";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
+import Lista from "../../components/lista/Lista";
 
 const CadastroFilme = () => {
     return(
